2c6a3daa0c67cab6be96a7c8f1a40358d4150c20
linhht <hoanglinh831999@gmail.com>
2021-06-02 21:10:41 +0700

    binary search

---
+++ search/binary_search.py
@@ -0,0 +1,17 @@
+def binary_search(arr, x):
+    """Find x in ordered array"""
+    low = 0
+    high = len(arr) - 1
+    mid = 0
+
+    while low <= high:
+        mid = (high + low) // 2
+
+        if arr[mid] < x:
+            low = mid + 1
+        elif arr[mid] > x:
+            high = mid - 1
+        else:
+            return mid
+
+    return -1
